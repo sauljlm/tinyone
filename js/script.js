2e3caaa34eff2b613a1f26e2a292e50e9b5e7404
Saul Lopez <slopez@LAB-DIC-3.local>
2018-11-27 10:11:36 -0600

    agrego responsibe mobile falta centrat algunas cosas

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-/**
- * Project Description.
- * ...
- */
-
-const text = 'Hola mundo!';
-const container = document.getElementById('container');
-const paragraph = document.createElement('p');
-paragraph.innerText = text;
-container.appendChild(paragraph);
+// funcion para el menu
+let btn = false;
+const hamburguer = document.getElementById('hamburguer');
+const menu = document.getElementById('menu');
+function showMenu(){
+    if (btn === false) {
+        menu.style.display = "block";
+        btn = true;
+    }else if (btn === true) {
+        menu.style.display = "none";
+        btn = false;
+    }
+}
+hamburguer.addEventListener('click', showMenu);
+// fin funcion para el menu
